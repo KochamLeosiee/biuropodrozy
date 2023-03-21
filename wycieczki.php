12203e8cbcc59aee2bcea0f55ba6547f9516d7cd
KochamLeosiee <99250960+KochamLeosiee@users.noreply.github.com>
2023-03-21 15:48:00 +0100

    Add files via upload

---
+++ wycieczki.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
+    <title>Wycieczki i urlopy</title>
+</head>
+<body>
+<header>
+        <h1>BIURO PODRÓŻY</h1>
+    </header>
+    <div id="lewy">
+        <h3>KONTAKT</h3>
+        <a href="mailto:biuro@wycieczki.pl?subject=Napisz do nas!">Napisz do nas</a>
+        <p>Telefon:555666777</p>
+    </div>
+    <div id="srodkowy">
+        <br>
+        <h3>GALERIA</h3><br>
+
+    </div>
+    <div id="prawy">
+        <h3>PROMOCJE</h3>
+        <table>
+        <tr>
+                <td>Jesień</td>
+                <td>Grupa 4+</td>
+                <td>Grupa 10+</td>
+            </tr>
+            <tr>
+                <td>5%</td>
+                <td>10%</td>
+                <td>15%</td>
+            </tr>
+        </table>
+    </div>
+    <div id="dane">
+        <h3>LISTA WYCIECZEK</h3>
+        <br>
+    </div>
+    <footer>
+        <p>Stronę wykonał Aleksander </p>
+    </footer>
+    
+</body>
+</html>
